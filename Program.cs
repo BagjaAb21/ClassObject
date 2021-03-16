b0bcab16bef794620ae4d639a6b5fda92290d34b
Bagja <bagja.basith0612@students.amikom.ac.id>
2021-03-16 18:49:28 +0700

    membuat code dengan memahami class dan object

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari classs mahasiswa
+            taxi driver = new taxi();
+
+            // mengeset nilai properties objek saya dan kamu
+            driver.nama = "Ramapatuh";
+            driver.status = "Yes";
+            driver.no_penumpang = "10";
+
+            //pemanggilan method
+
+            driver.Data();
+            driver.Status();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugas
+{
+    public class taxi
+    {
+        //properties
+        public string nama { get; set; }
+        public string status { get; set; }
+        public string no_penumpang { get; set; }
+
+        //Method
+        public void Data()
+        {
+            Console.WriteLine("Nama Driver: {0}", nama);
+            Console.WriteLine("On Duty: {0}", status);
+            Console.WriteLine("Number Of Passenger: {0}", no_penumpang);
+            Console.WriteLine("\n{0} sedang menjemput penumpang", nama);
+        }
+        public void Status()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", nama);
+            Console.WriteLine();
+        }
+    }
+}
